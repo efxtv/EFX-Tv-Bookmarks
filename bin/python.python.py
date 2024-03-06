b29926a8710cacbaefbf8ac4eb061814675a4239
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2024-03-06 20:21:16 +0530

    Update python.python.py

--- bin/python.python.py
+++ bin/python.python.py
@@ -1,1 +1,30 @@
+Topic
 
+NOTE: This post serves as a demonstration for creating a Python application. I do not endorse spamming or violating its terms of service of individual.
+
+Create Telegram bot FileName.py:
+
+⚡️ First, install the necessary libraries:
+```
+$ pip install depends
+```
+
+
+⚡️ Create a file Filename.py
+
+⚡️ Go to BotFather and create a new Telegram Bot
+
+⚡️ Save the script as Filename.py Source
+
+```
+SRIPT
+```
+
+
+⚡️ Execute the 'Filename.py' script, and visit your Telegram bot, type '/start' to receive the welcome message.
+
+⚡️ Discribe .
+
+#toolname
+#python_telegram_bot_source_codes
+#python_Telegram_Bot
