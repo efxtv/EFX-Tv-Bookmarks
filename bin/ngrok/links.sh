ea94e5448f16bd9c062589484e251abdf1ac1f5f
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2024-12-27 22:51:45 +0530

    Update links.sh

--- bin/ngrok/links.sh
+++ bin/ngrok/links.sh
@@ -11,7 +11,7 @@ Purple='\033[0;35m'
 Cyan='\033[0;36m'
 White='\033[0;37m'
 clear='\033[0m'
-/L3MON/ngrok/cloudflared-linux-amd64 tunnel -url localhost:5555 --logfile /L3MON/ngrok/.cf.log > /dev/null 2>&1 &
+/L3MON/ngrok/cloudflared-linux-amd64 tunnel -url 127.0.0.1:22533 --logfile /L3MON/ngrok/.cf.log > /dev/null 2>&1 &
 /L3MON/ngrok/ngrok start --all > /dev/null &
 sleep 8
 cdf=$(cat /L3MON/ngrok/.cf.log | grep -o 'https://[-0-9a-z]*\.trycloudflare.com')
