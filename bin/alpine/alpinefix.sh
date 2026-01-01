965e5ac1954865c22c3ba26fe84a9cba7403783e
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2026-01-01 06:35:17 -0500

    Update alpinefix.sh

--- bin/alpine/alpinefix.sh
+++ bin/alpine/alpinefix.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+apk update
+apk add docker nano
+rc-update add docker boot
+service docker start
+
+
+# Setup Root Profile
 cat <<EOF > /root/.profile
 export PS1="\[\e[92m\]▷ \[\e[31m\](\u@root) \w/# \[\e[m\]"
 alias ls='ls --color=auto'
 export SHELL=/bin/bash
 EOF
 
+# Setup User Template
 mkdir -p /etc/skel
 cat <<EOF > /etc/skel/.profile
 export PS1="\[\e[92m\]▷ \[\e[32m\](\u@root) \w/# \[\e[m\]"
 alias ls='ls --color=auto'
 export SHELL=/bin/bash
 EOF
-
-rc-update add docker boot
